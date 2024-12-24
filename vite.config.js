0f1f967a123d9e37968a05c4d03c2aa44e515623
Danil <danilser2005@gmail.com>
2024-12-24 09:05:13 +0500

    3

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cinephile',
+  base: '/Cinephile',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
