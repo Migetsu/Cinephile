231ef9aef84aff7fb69524875eecf801a1044cb4
Danil <danilser2005@gmail.com>
2024-12-31 00:28:26 +0500

    Updates

---
+++ assets/Films-Cctg683Q.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./ContentList-DDr_nq68.js";import{c as o,a as t,o as a}from"./index-DjeT7avg.js";const s={class:"main"},i={__name:"Films",setup(c){return(_,n)=>(a(),o("div",s,[t(e,{content:"movie"})]))}};export{i as default};
+//# sourceMappingURL=Films-Cctg683Q.js.map
---
+++ assets/Serials-DdRLrFvD.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./ContentList-DDr_nq68.js";import{c as a,a as s,o as t}from"./index-DjeT7avg.js";const o={class:"main"},m={__name:"Serials",setup(c){return(r,_)=>(t(),a("div",o,[s(e,{content:"serials"})]))}};export{m as default};
+//# sourceMappingURL=Serials-DdRLrFvD.js.map
